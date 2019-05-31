906ff4a8eff6d1c77a6f3533eeb019d3949e8346
Steve Dove <Stephen.Dove@caci.com>
2019-05-31 13:14:40 -0400

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+# Steve's new comment
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
